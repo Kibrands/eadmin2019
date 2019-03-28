145d3e9042e7ad9db069cb932b2b1ae42881e126
formacion <formacion@mvformacion-8>
2019-03-28 08:38:20 +0100

    first commit

---
+++ src/main/java/es/fpdual/eadmin/eadmin/repositorio/RepositorioDocumento.java
@@ -0,0 +1,17 @@
+package es.fpdual.eadmin.eadmin.repositorio;
+
+import java.util.List;
+
+import es.fpdual.eadmin.eadmin.modelo.Documento;
+
+public interface RepositorioDocumento {
+	public Documento altaDocumento(Documento documento);
+
+	public Documento modificarDocumento(Documento documento);
+
+	public void eliminarDocumento(int codigoDocumento);
+
+	public List<Documento> obtenerTodosDocumentos();
+
+	public int getSiguienteId();
+}
---
+++ src/main/java/es/fpdual/eadmin/eadmin/servicio/ServicioDocumento.java
@@ -0,0 +1,16 @@
+package es.fpdual.eadmin.eadmin.servicio;
+
+import java.util.List;
+
+import es.fpdual.eadmin.eadmin.modelo.Documento;
+
+public interface ServicioDocumento {
+
+	public Documento altaDocumento(Documento documento);
+
+	public Documento modificarDocumento(Documento documento);
+
+	public void eliminarDocumento(int codigoDocumento);
+
+	public List<Documento> obtenerTodosDocumentos();
+}
